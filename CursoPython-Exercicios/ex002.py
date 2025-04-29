356a435cb919b081e3f27e8c333fdbcec5785f65
Victor Ponciano <victordias.ponciano@gmail.com>
2025-04-29 11:51:24 -0300

    Segundo exercício realizado e correto

---
+++ CursoPython-Exercicios/ex002.py
@@ -0,0 +1,6 @@
+# EXC 2 - Faça um programa que leia o nome de uma pessoa e exiba uma mensagem de boas vindas
+
+nome = input('Digite seu nome: ')
+
+print('Boas vindas', nome, '! Este é um código python')
+print('Boas vindas, {}! Este é um código Python com saída formatada'.format(nome)) # Saída formatada
