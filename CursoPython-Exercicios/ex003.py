13b76f9159051847593fd8ca8d5516fba2b0b01f
Victor Ponciano <victordias.ponciano@gmail.com>
2025-04-30 09:43:15 -0300

    Terceiro exercício realizado e corrigido

---
+++ CursoPython-Exercicios/ex003.py
@@ -0,0 +1,19 @@
+# EXC 3 - Crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Digite um número: '))
+
+n2 = int(input('Digite outro número: '))
+
+soma = n1 + n2
+
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
+
+# Adicional - Divisão entre dois números
+
+n3 = float(input(''))
+
+n4 = float(input(''))
+
+q = n3 / n4
+
+print('A divisão entre {} e {} é igual a {}'.format(n3, n4, q))
