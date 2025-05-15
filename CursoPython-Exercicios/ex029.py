1d76919e5ffc87a3a836aeaf5b7dd71fc3fd4cd1
Victor Ponciano <victordias.ponciano@gmail.com>
2025-05-15 14:46:28 -0300

    Exercício 29 atualizado e modificado

--- CursoPython-Exercicios/ex029.py
+++ CursoPython-Exercicios/ex029.py
@@ -1,12 +1,14 @@
 """ EXC 29 - Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mesnagem dizendo que ele foi multado. A multa vai custar R$7,00 para cada km acima do limite """
 
-from rich import print # A biblioteca rich tem o papel de colorir textos
+cores = {'limpatela': '\033[0m',
+         'red':'\033[1;31m',
+         'green':'\033[1;32m'}
 
 vel = int(input('Digite a velocidade de seu carro no radar: '))
 
 if vel > 80:
     multa = vel - 80
-    print(f"[red]Você foi multado! O valor da multa é: R${multa * 7},00[/red]")
+    print(f"{cores['red']}Você foi multado! O valor da multa é: R${multa * 7},00{cores['limpatela']}")
 # Esse else é desnecessário (basta só exibir a multa - pelo menos na minha visão)
 else:
-    print('[green]Estava dentro do limite. Lembre-se de dirigir com segurança![/green]')
+    print(f'{cores["green"]}Estava dentro do limite. Lembre-se de dirigir com segurança!{cores["limpatela"]}')
