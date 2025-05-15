041c90c3431341414bfbba1d10d26cbae1f2f02d
Victor Ponciano <victordias.ponciano@gmail.com>
2025-05-15 09:33:31 -0300

    Exercício 30 finalizado com o uso de cores para exibição de resultados

--- CursoPython-Exercicios/ex030.py
+++ CursoPython-Exercicios/ex030.py
@@ -1,8 +1,10 @@
 # EXC 30 - Crie um programa que leia um número inteiro e exiba se ele é PAR ou ÍMPAR
 
+from rich import print
+
 n = int(input('Digite um número: '))
 
 if (n % 2 == 0):
-    print('Número par')
+    print('[green]Número par[/green]')
 else:
-    print('Número ímpar')
+    print('[red]Número ímpar[/red]')
