ed607f0baa05b23b12b2012e48f46a3abc616139
Victor Ponciano <victordias.ponciano@gmail.com>
2025-05-15 09:30:39 -0300

    Exercício 30 finalizado

---
+++ CursoPython-Exercicios/ex030.py
@@ -0,0 +1,8 @@
+# EXC 30 - Crie um programa que leia um número inteiro e exiba se ele é PAR ou ÍMPAR
+
+n = int(input('Digite um número: '))
+
+if (n % 2 == 0):
+    print('Número par')
+else:
+    print('Número ímpar')
